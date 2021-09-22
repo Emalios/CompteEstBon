03d39645b1930e1b1acd94751e384b7e0ed57e14
auwnzk <tomcondenseau@gmail.com>
2021-09-22 20:58:08 +0200

    ajout d'une methode qui permet de calculer la moyenne d'iterations pour résoudre n problèmes, permettant d'optimiser plus facilement la methode principale

--- src/Nombres.java
+++ src/Nombres.java
@@ -2,28 +2,31 @@ import java.util.*;
 
 public class Nombres {
 
-    private static Map<List<Integer>,Integer> nombres;
-    private final List<Integer> valeurs = List.of(1,2,3,4,5,6,7,8,9,10,25,50,75,100);
+    private static Map<List<Integer>, Integer> nombres;
+    private final List<Integer> valeurs = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 25, 50, 75, 100);
 
-    public Nombres(){
+    public Nombres() {
         nombres = new HashMap<>();
     }
 
-    public void ajouterProbleme(){
+    public void ajouterProbleme() {
         List<Integer> valeursChoisies = new ArrayList<>();
         int nombreAleatoire;
-        for(int i=0;i<6;i++){
-            nombreAleatoire = (int)(Math.random() * ((13) + 1));
+        for (int i = 0; i < 6; i++) {
+            nombreAleatoire = (int) (Math.random() * ((13) + 1));
             valeursChoisies.add(valeurs.get(nombreAleatoire));
         }
-        int nombreFinal =101 + (int)(Math.random() * ((999 - 101) + 1));
-        nombres.put(valeursChoisies,nombreFinal);
+        int nombreFinal = 101 + (int) (Math.random() * ((999 - 101) + 1));
+        nombres.put(valeursChoisies, nombreFinal);
     }
 
-    public void toStringNombres(){
-        for (List list: nombres.keySet()) {
-            System.out.println("Nombres: "+list+"\nRésulat souhaité: "+nombres.get(list)+"\n");
+    public void toStringNombres() {
+        for (List list : nombres.keySet()) {
+            System.out.println("Nombres: " + list + "\nRésulat souhaité: " + nombres.get(list) + "\n");
         }
     }
 
+    public Map<List<Integer>, Integer> getNombres() {
+        return this.nombres;
+    }
 }
