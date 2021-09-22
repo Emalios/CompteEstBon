6e8ba41aab0b40dc86df26c7f5579d052c1e98df
BERGERAT Hugo <hugo.bergerat2002@gmail.com>
2021-09-22 15:10:55 +0200

    V1 de l'algorithme ne cherchant pas le résultat le plus proche si aucun chemin trouvé vers le résultat attendu, 1 216 513 appels récursifs sur Exemple 1.

--- src/CompteEstBon.java
+++ src/CompteEstBon.java
@@ -9,11 +9,14 @@ public class CompteEstBon {
     private static BinaryOperator<Integer> MUL = (integer, integer2) -> integer * integer2;
     private static BinaryOperator<Integer> DIV = (integer, integer2) -> integer / integer2;
 
+    private static int NB_APPEL = 0;
+
     public static void main(String[] args) {
         List<Integer> nombres = List.of(9, 1, 6, 8, 2, 3);
         int attendu = 845;
         List<BinaryOperator<Integer>> operations = List.of(PLUS, MINUS, MUL, DIV);
-        System.out.println(genererPossibilites(List.of(1, 2, 3)));
+        rechercherSolution(nombres, operations, attendu);
+        System.out.println("Nb appel: " + NB_APPEL);
     }
 
     public static boolean rechercherSolution(List<Integer> nombres, List<BinaryOperator<Integer>> operations, int attendu) {
@@ -26,24 +29,36 @@ public class CompteEstBon {
             int indiceNombres = 0;
             int indiceOperation = 0;
             List<Pair<Integer, Integer>> possibilites = genererPossibilites(nombres);
-            while (infructueux && indiceNombres < possibilites.size()) {
+            while (infructueux && indiceNombres < possibilites.size() - 1) {
                 Pair<Integer, Integer> possibilite = possibilites.get(indiceNombres);
                 int nombre1 = possibilite.getFirst();
                 int nombre2 = possibilite.getSecound();
                 BinaryOperator<Integer> operation = operations.get(indiceOperation);
-                int resultat = operation.apply(nombre1, nombre2);
-                if(acceptable(nombre1, nombre2, operation, resultat)) {
+                if(acceptable(nombre1, nombre2, operation)) {
+                    int resultat = operation.apply(nombre1, nombre2);
                     List<Integer> newNombres = new ArrayList<>(nombres);
-                    newNombres.remove(nombre1);
-                    newNombres.remove(nombre2);
+                    newNombres.remove(newNombres.indexOf(nombre1));
+                    newNombres.remove(newNombres.indexOf(nombre2));
                     newNombres.add(0, resultat);
+                    //System.out.println("Essaie avec: " + nombre1 + afficherOperation(operation) + nombre2 + "=" + resultat);
+                    NB_APPEL++;
                     infructueux = rechercherSolution(newNombres, operations, attendu);
                     if(infructueux) {
-                        if(indiceOperation < operations.size()) indiceOperation++;
+                        //System.out.println("Infructueux avec: " + nombre1 + afficherOperation(operation) + nombre2 + "=" + resultat);
+                        if(indiceOperation < operations.size() - 1) indiceOperation++;
                         else {
                             indiceOperation = 0;
                             indiceNombres++;
                         }
+                    } else {
+                        System.out.println(nombre1 + afficherOperation(operation) + nombre2 + "=" + resultat);
+                    }
+                } else {
+                    //System.out.println("Pas accessible avec: " + nombre1 + afficherOperation(operation) + nombre2);
+                    if(indiceOperation < operations.size() - 1) indiceOperation++;
+                    else {
+                        indiceOperation = 0;
+                        indiceNombres++;
                     }
                 }
             }
@@ -51,9 +66,11 @@ public class CompteEstBon {
         return infructueux;
     }
 
-    private static boolean acceptable(int nombre1, int nombre2, BinaryOperator<Integer> operation, int resultat) {
-        if(resultat < 0) return false;
+    private static boolean acceptable(int nombre1, int nombre2, BinaryOperator<Integer> operation) {
         if(nombre2 == 0 && operation.equals(DIV)) return false;
+        //if(((nombre1 == 1) || (nombre2 == 1)) && (operation.equals(DIV) || operation.equals(MUL))) return false;
+        int resultat = operation.apply(nombre1, nombre2);
+        if(resultat < 0) return false;
         return true;
     }
 
@@ -72,8 +89,8 @@ public class CompteEstBon {
 
     private static List<Pair<Integer, Integer>> genererPossibilites(List<Integer> nombres) {
         List<Pair<Integer, Integer>> pairs = new ArrayList<>();
-        for(int i=0;i< nombres.size()-1;i++){
-            for(int j=0;j<nombres.size()-1;j++){
+        for(int i=0; i < nombres.size(); i++){
+            for(int j=0; j <nombres.size(); j++){
                 if(i!=j){
                     pairs.add(new Pair<>(nombres.get(i),nombres.get(j)));
                 }
