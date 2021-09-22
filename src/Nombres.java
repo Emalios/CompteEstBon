77ba71e3ccac1ccbc4486bef7dc3cf7435e11cfa
auwnzk <tomcondenseau@gmail.com>
2021-09-22 20:11:41 +0200

    ajout des méthodes ajouterProbleme et toStringNombres ajouterProbleme permet d'ajouter une suite de 6 nombres et le nombre final voulu dans une Map toStringNombres permet d'afficher tous les éléments de la map.

--- src/Nombres.java
+++ src/Nombres.java
@@ -1,26 +1,29 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Nombres {
 
-    private List<Map<List<Integer>,Integer>> nombres;
+    private static Map<List<Integer>,Integer> nombres;
     private final List<Integer> valeurs = List.of(1,2,3,4,5,6,7,8,9,10,25,50,75,100);
 
     public Nombres(){
-        nombres = new ArrayList<>();
+        nombres = new HashMap<>();
     }
 
     public void ajouterProbleme(){
         List<Integer> valeursChoisies = new ArrayList<>();
-        for(int i=0;i<=6;i++){
-            int nombreAleatoire = (int)(Math.random() * ((14) + 1));
+        int nombreAleatoire;
+        for(int i=0;i<6;i++){
+            nombreAleatoire = (int)(Math.random() * ((13) + 1));
             valeursChoisies.add(valeurs.get(nombreAleatoire));
         }
         int nombreFinal =101 + (int)(Math.random() * ((999 - 101) + 1));
-        Map map = new HashMap();
-        map.put(valeursChoisies,nombreFinal);
-        nombres.add(map);
+        nombres.put(valeursChoisies,nombreFinal);
     }
+
+    public void toStringNombres(){
+        for (List list: nombres.keySet()) {
+            System.out.println("Nombres: "+list+"\nRésulat souhaité: "+nombres.get(list)+"\n");
+        }
+    }
+
 }
