fc0017ceff95d8f8df62977a70ce05324d9a29bc
Emalios <35004924+Emalios@users.noreply.github.com>
2021-10-12 13:43:28 +0200

    Début d'implémentation de la partie 2

--- src/CompteEstBon.java
+++ src/CompteEstBon.java
@@ -1,21 +1,55 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.function.BinaryOperator;
 
 public class CompteEstBon {
 
+    /**
+     * représente l'addition
+     */
     private static final BinaryOperator<Integer> PLUS = (integer, integer2) -> integer + integer2;
+    /**
+     * représente a soustraction
+     */
     private static final BinaryOperator<Integer> MINUS = (integer, integer2) -> integer - integer2;
+    /**
+     * représente la multiplcation
+     */
     private static final BinaryOperator<Integer> MUL = (integer, integer2) -> integer * integer2;
+    /**
+     * représente la division
+     */
     private static final BinaryOperator<Integer> DIV = (integer, integer2) -> integer / integer2;
-    public static final List<BinaryOperator<Integer>> LIST_OPERATIONS = List.of(MUL, PLUS, DIV, MINUS);
+    /**
+     * liste de toutes les opérations
+     */
+    public static final List<BinaryOperator<Integer>> LIST_OPERATIONS = List.of(MUL, DIV, PLUS, MINUS);
 
+    /**
+     * liste représentant les calculs fait
+     */
     private static List<String> calculs = new ArrayList<>();
+
+    /**
+     * liste représentant les calculs fait
+     */
+    private static List<String> calculsTest = new ArrayList<>();
+
+    private static Pair<BinaryOperator<Integer>, Integer> NOMBRE_PLUS_PROCHE = new Pair<>(null, 0);
+    /**
+     * liste représentant les calculs le plus proche fait
+     */
+    private static List<Pair<BinaryOperator<Integer>, Integer>> calculsPlusProche = new ArrayList<>();
+
+    /**
+     * variable représentant le nombre d'appel à rechercherSolution
+     */
     private static int NB_APPEL = 0;
 
     public static void main(String[] args) {
-        List<Integer> nombres = new ArrayList<>(List.of(3, 75, 1, 4, 5, 2));
-        int attendu = 598;
+        List<Integer> nombres = new ArrayList<>(List.of(1, 3, 5, 1, 10, 3, 2));
+        int attendu = 973;
         boolean infructueux = rechercherSolution(nombres, LIST_OPERATIONS, attendu)._1();
         if(!infructueux) {
             System.out.println("Le compte est bon!!");
@@ -23,15 +57,21 @@ public class CompteEstBon {
             calculs.forEach(System.out::println);
         } else {
             System.out.println("Pas de solutions exacte.");
-
+            System.out.println("Calcul :");
+            calculsPlusProche.forEach(binaryOperatorIntegerPair -> {
+                System.out.println(afficherOperation(binaryOperatorIntegerPair._1()) + " = " + binaryOperatorIntegerPair._2());
+            });
         }
         System.out.println("Nb appel: " + NB_APPEL);
         System.out.println("Commencement test");
-        Nombres.faireTourner(200);
+        //Nombres.faireTourner(2000);
     }
 
     public static Pair<Boolean, Integer> rechercherSolution(List<Integer> nombres, List<BinaryOperator<Integer>> operations, int attendu) {
         NB_APPEL++;
+        Pair<BinaryOperator<Integer>, Integer> plusProches = new Pair<>(NOMBRE_PLUS_PROCHE._1(), NOMBRE_PLUS_PROCHE._2());
+        int nbPlusProche1 = 0;
+        int nbPlusProche2 = 0;
         boolean infructueux;
         if(nombres.contains(attendu)) {
             infructueux = false;
@@ -54,27 +94,39 @@ public class CompteEstBon {
                     newNombres.remove(newNombres.indexOf(nombre1));
                     newNombres.remove(newNombres.indexOf(nombre2));
                     newNombres.add(0, resultat);
-                    //System.out.println("Essaie avec: " + nombre1 + afficherOperation(operation) + nombre2 + "=" + resultat);
                     infructueux = rechercherSolution(newNombres, operations, attendu)._1();
+                    //test
+                    if (Math.abs(resultat - attendu) < Math.abs(plusProches._2() - attendu)) {
+                        nbPlusProche1 = nombre1;
+                        nbPlusProche2 = nombre2;
+                        plusProches = new Pair<>(operation, resultat);
+                    }
+                    //fintest
                     if(infructueux) {
-                        //System.out.println("Infructueux avec: " + nombre1 + afficherOperation(operation) + nombre2 + "=" + resultat);
+                        //si on a pas essayé toutes les opérations on essaie la suivante
                         if(indiceOperation < operations.size() - 1) indiceOperation++;
+                        //sinon on reset et on change de nombre
                         else {
                             indiceOperation = 0;
                             indiceNombres++;
                         }
                     } else {
+                        //ajout à l'indice 0 afin d'avoir le bon ordre
                         calculs.add(0, nombre1 + afficherOperation(operation) + nombre2 + " = " + resultat);
                     }
                 } else {
-                    //System.out.println("Pas accessible avec: " + nombre1 + afficherOperation(operation) + nombre2);
+                    //si on a pas essayé toutes les opérations on essaie la suivante
                     if(indiceOperation < operations.size() - 1) indiceOperation++;
+                    //sinon on reset et on change de nombre
                     else {
                         indiceOperation = 0;
                         indiceNombres++;
                     }
                 }
             }
+            //System.out.println("ajout de : " + plusProches);
+            System.out.println("add");
+            calculsTest.add(0, nbPlusProche1 + afficherOperation(plusProches._1()) + nbPlusProche2 + " = " + plusProches._2());
         }
         return new Pair<>(infructueux, NB_APPEL);
     }
